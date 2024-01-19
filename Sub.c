e0013be76f7c9a3b8e5fe3bbef09f82984460466
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-01-19 19:41:56 +0530

    Sub.c

---
+++ Sub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int first,second,total;
+  printf("enter first number:");
+  scanf("%d",&first);
+  printf("enter second number:");
+  scanf("%d",&second);
+  total=first-second;
+  printf("Total value is %d:",total);
+}
