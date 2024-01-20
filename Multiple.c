a34f0c0fed9920fd30de2828b7540ef0453cf156
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-01-20 19:14:13 +0530

    Multiple.c

---
+++ Multiple.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int first,second,total;
+  printf("enter first number:");
+  scanf("%d",&first);
+  printf("enter second number:");
+  scanf("%d",&second);
+  Multiple=first*second;
+  printf("Multiple is %d:",Multiple);
+}
