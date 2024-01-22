51741f790dacedc66ba8b4cebd19bba2862f5325
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-01-22 19:47:32 +0530

    Create Div.c

---
+++ Div.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int first,second,Division;
+  printf("enter first number:");
+  scanf("%d",&first);
+  printf("enter second number:");
+  scanf("%d",&second);
+  Division=first/second;
+  printf("Division of the value %d:",Division);
+}
