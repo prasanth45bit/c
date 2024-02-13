6d8b301e0df3e57c9f97d0522a48ee823b496c35
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-02-13 07:52:55 +0530

    Game1.c
    Game number 1

---
+++ Game1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter a value:");
+scanf("%d",&a);
+printf("enter b value:");
+scanf("%d",&b);
+if(a>=0 && a<=2 && b>=0 && b<=2)
+{
+    if(a==b)
+    {
+    printf("match draw");
+    }
+    if else(a>b)
+    {
+    printf("a wins");
+    }
+    else
+    {
+    printf("b wins");
+    }
+}
+else
+{
+printf("invalid");
+}
+}
