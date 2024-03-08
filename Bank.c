97348d5e5f0981dec2d3b4fa7d7181bbbb2d42f7
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-03-08 19:23:13 +0530

    Bank.c

--- Bank.c
+++ Bank.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-3
-4. int main() f
-5
-printf("********************BANK MANAGEMENT SYSTEM**********
-printf("In1. CREATE NEW ACCOUNT");
-7
-printf("In2. DEPOSIT AMOUNT");
-8
-printf("In3.WITHDRAW AMOUNT");
-printf("In4.BALANCE ENQUIRY");
-printf("In5.ALL ACCOUNT HOLDER LIST");
-printf("In6.CLOSE AN ACCOUNT");
-printf("In7.MODIFY AN ACCOUNT");
-printf("In8.EXIT");
-A
-printf("InSelect Your Option (1-8) ");
-12
-printf("In1");
-printf("In***Create new Account***");
-printf("InEnter new account number:");
-printf("InEnter initial amount to ber deposited: 22500");
-printf("InAmount Has been added successfully");
-21
-F
+int main() 
+{
+printf("**********BANK MANAGEMENT SYSTEM**********");
+printf("\n1. CREATE NEW ACCOUNT");
+printf("\n2. DEPOSIT AMOUNT");
+printf("\n3.WITHDRAW AMOUNT");
+printf("\n4.BALANCE ENQUIRY");
+printf("\n5.ALL ACCOUNT HOLDER LIST");
+printf("\n6.CLOSE AN ACCOUNT");
+printf("\n7.MODIFY AN ACCOUNT");
+printf("\n8.EXIT");
+printf("\nSelect Your Option (1-8) ");
+printf("\n1");
+printf("\n***Create new Account***");
+printf("\nEnter new account number:");
+printf("\nEnter initial amount to ber deposited: 22500");
+printf("\nAmount Has been added successfully");
+
