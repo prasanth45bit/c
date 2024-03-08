637d271cebddc6988051a40efba4da2f68466d70
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-03-08 19:20:45 +0530

    Bank.c

---
+++ Bank.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+3
+4. int main() f
+5
+printf("********************BANK MANAGEMENT SYSTEM**********
+printf("In1. CREATE NEW ACCOUNT");
+7
+printf("In2. DEPOSIT AMOUNT");
+8
+printf("In3.WITHDRAW AMOUNT");
+printf("In4.BALANCE ENQUIRY");
+printf("In5.ALL ACCOUNT HOLDER LIST");
+printf("In6.CLOSE AN ACCOUNT");
+printf("In7.MODIFY AN ACCOUNT");
+printf("In8.EXIT");
+A
+printf("InSelect Your Option (1-8) ");
+12
+printf("In1");
+printf("In***Create new Account***");
+printf("InEnter new account number:");
+printf("InEnter initial amount to ber deposited: 22500");
+printf("InAmount Has been added successfully");
+21
+F
